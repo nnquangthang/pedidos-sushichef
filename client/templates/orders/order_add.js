56c31bbc4965ef6b97a3d67169eae9be2e11dad9
thangNguyen <nnquangthang@gmail.com>
2015-11-07 13:45:46 +0700

    Fully function order_add

--- client/templates/orders/order_add.js
+++ client/templates/orders/order_add.js
@@ -1,39 +1,76 @@
 Template.orderAdd.rendered=function(){
+  // Datepicker
+  $('.datepicker').pickadate({
+  format: 'mm/dd/yyyy',
+    // Blur function onClose event for Semantic-UI Form Validations
+    onClose: function(){
+      $('.datepicker').blur();
+    }
+  });
+  // Dropdown
   $('select.dropdown').dropdown();
-  $('.datepicker').pickadate({format: 'dd-mm-yyyy'});
+  // Form validations
   $('.ui.form').form({
+    on: 'blur',
+    inline: true,
     fields:{
       quantity: {
         identifier: 'quantity',
-        rules: [{type:'minCount[1]',prompt : 'Please enter Quantity'}]
+        rules: [{type:'empty',prompt : 'Enter quantity'}]
       },
       fishes:{
         identifier: 'fishes',
-        rules: [{type: 'minCount[1]', prompt: 'Plese choose at least one'}]
+        rules: [{type: 'minCount[1]', prompt: 'At least one fish'}]
       },
       mat:{
         identifier: 'mat',
-        rules: [{type: 'minCount[1]', prompt: 'Plese choose mat'}]
+        rules: [{type: 'empty', prompt: 'Yes or No?'}]
       },
       date:{
         identifier: 'date',
-        rules: [{type: 'minCount[1]', prompt: 'Plese choose a date to deliver'}]
+        rules: [{type: 'empty', prompt: 'Date?'}]
       }
-
-    }
+    },
   })
 }
 
 Template.orderAdd.events({
   'submit form': function(event){
     event.preventDefault();
-    var quantity = $('[name=quantity]').val();
-    console.log(quantity);
-    var fishes = $('[name=fishes]').val();
-    console.log(fishes);
-    var mat = $('[name=mat]').val();
-    console.log(mat);
-    var date = $('[name=date]').val();
-    console.log(date);
+    if(!Meteor.user()){
+      console.log('Not login');
+    }else{
+    // Get form values
+    order = $('[name=orderAdd]').form('get values');
+    // Declare varibles
+    var userId = Meteor.user()._id;
+    var quantity = order.quantity;
+    var fishes = order.fishes;
+    var mat = order.mat;
+    var date = new Date(order.date);
+    // Insert to collection.Orders
+    Orders.insert({
+      userId: userId,
+      quantity: quantity,
+      fishes: fishes,
+      mat: mat,
+      date: date,
+      createdAt: new Date()
+    },
+    // Catch error, result
+    function(error, result){
+      if(error){
+        Bert.alert('Something wrong happened! Please order again!', 'danger', 'growl-top-right' );
+      };
+      if(result){
+        $('[name=orderAdd]').form('clear');
+        Bert.alert('Your order has been successfully created', 'success', 'growl-top-right' );
+      };
+    });
+
+    }
+  },
+  'click #clearButton': function(){
+    $('[name=orderAdd]').form('clear');
   }
 })
